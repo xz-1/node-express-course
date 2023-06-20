3f95e482ac6024c9a775e86cc018dc2443b2d60d
xz-1 <skonchan199w@gmail.com>
2023-06-19 23:18:12 -0400

    commit router parameter

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -11,9 +11,45 @@ app.get('/', (req, res) => {
 
 
 app.get('/api/products', (req, res) => {
-
+    const newProduct = products.map((product) => {
+        const { id, name, image } = product;
+        return { id, name, image }
+    })
+    res.json(newProduct)
+    //res.json(products)
 })
 
+
+// app.get('/api/products/1', (req, res) => {
+//     const singleProduct = products.find((product) => product.id === 1)
+//     res.json(singleProduct)
+// })
+
+//route in this case (Node.JS) think of it as the Place Holder
+//in this case is ':productID' in the URL and 'req.params' in the Code to access the data and setup somekind of logic
+app.get('/api/products/:productID', (req, res) => {
+    // console.log(req)
+    // console.log(req.params)
+    const { productID } = req.params
+    const singleProduct = products.find((product) => product.id === Number(productID))
+
+    if (!singleProduct) {
+        return res.status(404).send('Product Does Not Exist')
+    }
+    //check the result in terminal for debuging purpose
+    //console.log(singleProduct)
+
+    //send back res object to broswer
+    return res.json(singleProduct)
+})
+
+//one more example of Route params
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+    console.log(req.params)
+    const { productID, reviewID } = req.params
+    console.log(`product id: ${productID} review id: ${reviewID}`)
+    res.send('Hello World')
+})
 
 
 app.listen(5000, () => {
